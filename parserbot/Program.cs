1c724be372a8d04f4aaba79718551fe27a197246
VivoBook <VivoBook@DESKTOP-B1HFRTK>
2023-10-05 10:34:08 +0300

    05-04-2023(1)

--- parserbot/Program.cs
+++ parserbot/Program.cs
@@ -8,12 +8,13 @@ using System.Threading;
 using Telegram.Bot;
 using Telegram.Bot.Types;
 using Telegram.Bot.Polling;
+using OpenQA.Selenium.Chrome;
 
 namespace parserbot
 {
     class Program
     {
-        static ITelegramBotClient bot = new TelegramBotClient("------------------------------------");
+        static ITelegramBotClient bot = new TelegramBotClient("6384813785:AAEt5oFkXLxjQE6WA4Cm4guvUtrVTnwlDbg");
 
         static void Main(string[] args)
         {
@@ -34,43 +35,46 @@ namespace parserbot
             Console.ReadLine();
         }
 
-        public async Task WeDriverRun()
+        public static string WebDriverRun(string url)
         {
+            String result = "";
             IWebDriver driver;
 
             // Дайвер путь
-            string path = "C:\\Users\\shymkentbay.b\\source\\repos\\parserbot\\parserbot\\drivers";
+            string path = "C:\\distr\\drivers";
 
-            EdgeOptions options = new EdgeOptions();
+            var options = new ChromeOptions();
             options.AddArgument("--start-maximized");
             // Создать экземплярь драйвера
-            driver = new EdgeDriver(path, options);
+            driver = new ChromeDriver(path, options);
             // Запуск браузера
-            driver.Navigate().GoToUrl("https://kaspi.kz/shop/p/apple-iphone-13-128gb-chernyi-102298404/?c=750000000");
+            //driver.Navigate().GoToUrl("https://kaspi.kz/shop/p/apple-iphone-13-128gb-chernyi-102298404/?c=750000000");
+            driver.Navigate().GoToUrl(url);
             driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromMilliseconds(2000);
             try
             {
-                //var head = driver.FindElement(By.ClassName("item__heading"));
+                var head = driver.FindElement(By.ClassName("item__heading"));
+                result = head.Text;
                 //var rate = driver.FindElement(By.ClassName("item__rating-link"));
-                var price = driver.FindElement(By.ClassName("item__price-once"));
-                if (price == null)
-                {
-                    Console.WriteLine("price = null");
-                }
-                else
-                    Console.WriteLine("Цена: " + price.Text);
+                //var price = driver.FindElement(By.ClassName("item__price-once"));
+                //if (price == null)
+                //{
+                //    Console.WriteLine("price = null");
+                //}
+                //else
+                //    Console.WriteLine("Цена: " + price.Text);
                 //Console.WriteLine("Рейтинг: " + driver.FindElement(By.ClassName("item__rating-link")).Text);
                 //Console.WriteLine("Цена: " + driver.FindElement(By.ClassName("item__price-once")).Text);
             }
             catch (Exception ex)
             {
                 Console.WriteLine("\n*************************** Error ****************************************");
-                Console.WriteLine();
             }
             finally
             {
                 driver.Close();
             }
+            return result;
         }
 
         public static async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
@@ -85,7 +89,12 @@ namespace parserbot
                     await botClient.SendTextMessageAsync(message.Chat, "Добро пожаловать на борт, добрый путник!");
                     return;
                 }
-                await botClient.SendTextMessageAsync(message.Chat, "Привет-привет!!");
+                else if(message.Text.Length > 16 && message.Text.Substring(0, 17) == "https://kaspi.kz/")
+                {
+                    await botClient.SendTextMessageAsync(message.Chat, WebDriverRun(message.Text));
+                    return;
+                }
+                await botClient.SendTextMessageAsync(message.Chat, "Добро пожаловать в наш бот Каспи анализ!");
             }
         }
 
